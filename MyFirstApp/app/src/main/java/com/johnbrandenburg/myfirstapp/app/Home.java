cb1a78d2461b21cc76bdb0c8132a976b6d4b3978
John Brandenburg <jmbburg26@fullsail.edu>
2014-06-06 08:02:20 +0300

    Added layout and onClick listener
    Set up the input fields and button for user to enter values. Clicking
    button calculates the values and present the total into a textView

---
+++ MyFirstApp/app/src/main/java/com/johnbrandenburg/myfirstapp/app/Home.java
@@ -0,0 +1,81 @@
+package com.johnbrandenburg.myfirstapp.app;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class Home extends ActionBarActivity {
+
+    Button calcButton;
+    TextView resultsView;
+    EditText totalAdults;
+    EditText totalTeens;
+    EditText totalChildren;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_layouts);
+
+        //Find my view elements
+        calcButton  =   (Button)    findViewById(R.id.calcButton);
+        resultsView =   (TextView)  findViewById(R.id.resultView);
+        totalAdults =   (EditText)  findViewById(R.id.totalAdults);
+        totalTeens  =   (EditText)  findViewById(R.id.totalTeens);
+        totalChildren = (EditText)  findViewById(R.id.totalChildren);
+
+
+
+        calcButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Get values of people inputted
+                String numAdults = totalAdults.getText().toString();
+                int finalAdults = Integer.parseInt(numAdults);
+
+                String numTeens = totalTeens.getText().toString();
+                int finalTeens = Integer.parseInt(numTeens);
+
+                String numChildren = totalChildren.getText().toString();
+                int finalChildren = Integer.parseInt(numChildren);
+
+                //Add all input values and calculate total number of people present
+                int totalPresent = finalAdults + finalChildren + finalTeens;
+
+                //Display the results of people enter into the counter
+                //resultsView.setText("There are " + finalAdults + " adults, " + finalTeens + " teens, and " + finalChildren + " children" + " at church today.");
+                resultsView.setText("There are " + totalPresent + " people in church today.");
+
+                //If statement to determine if you had a good day in church
+            }
+        });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
